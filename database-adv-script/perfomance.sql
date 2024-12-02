0aac9611e12a15437225db8e35318422782b0f04
Oualid-Ouarhil <ouarhiloualid@gmail.com>
2024-12-02 21:47:48 +0100

    REMADE

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,14 +1,16 @@
 
--- Refactored Query: Optimized for performance
 SELECT 
     Booking.booking_id,
     User.first_name AS user_first_name,
     User.last_name AS user_last_name,
+    User.email AS user_email,
     Property.name AS property_name,
     Property.location AS property_location,
+    Property.pricepernight,
     Payment.payment_id,
     Payment.amount AS payment_amount,
-    Payment.payment_method
+    Payment.payment_method,
+    Payment.payment_date
 FROM 
     Booking
 INNER JOIN 
@@ -16,7 +18,28 @@ INNER JOIN
 INNER JOIN 
     Property ON Booking.property_id = Property.property_id
 INNER JOIN 
-    Payment ON Booking.booking_id = Payment.booking_id
-WHERE
-    Booking.status = 'confirmed';  -- Example: filtering for confirmed bookings
+    Payment ON Booking.booking_id = Payment.booking_id;
 
+
+
+EXPLAIN ANALYZE 
+SELECT 
+    Booking.booking_id,
+    User.first_name AS user_first_name,
+    User.last_name AS user_last_name,
+    User.email AS user_email,
+    Property.name AS property_name,
+    Property.location AS property_location,
+    Property.pricepernight,
+    Payment.payment_id,
+    Payment.amount AS payment_amount,
+    Payment.payment_method,
+    Payment.payment_date
+FROM 
+    Booking
+INNER JOIN 
+    User ON Booking.user_id = User.user_id
+INNER JOIN 
+    Property ON Booking.property_id = Property.property_id
+INNER JOIN 
+    Payment ON Booking.booking_id = Payment.booking_id;
