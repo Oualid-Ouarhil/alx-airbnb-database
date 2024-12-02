f1393e623f635215ce68429b45fb8c980367fc46
Oualid-Ouarhil <ouarhiloualid@gmail.com>
2024-12-02 21:29:50 +0100

    FETCHING

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,37 @@
+SELECT 
+    Booking.booking_id,
+    Booking.start_date,
+    Booking.end_date,
+    Booking.status,
+    User.first_name,
+    User.last_name,
+    User.email
+FROM 
+    Booking
+INNER JOIN 
+    User ON Booking.user_id = User.user_id;
+
+
+SELECT 
+    Property.property_id,
+    Property.name,
+    Property.location,
+    Review.rating,
+    Review.comment
+FROM 
+    Property
+LEFT JOIN 
+    Review ON Property.property_id = Review.property_id;
+
+
+SELECT 
+    User.user_id,
+    User.first_name,
+    User.last_name,
+    Booking.booking_id,
+    Booking.start_date,
+    Booking.end_date
+FROM 
+    User
+FULL OUTER JOIN 
+    Booking ON User.user_id = Booking.user_id;
